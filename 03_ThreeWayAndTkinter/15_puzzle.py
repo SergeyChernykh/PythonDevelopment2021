2423d16404ddce1cb2a970cbd9322e85245b9fa2
SChernykh <s9160605122@gmail.com>
2021-03-14 17:09:51 +0300

    Add logic for numerics button

--- 03_ThreeWayAndTkinter/15_puzzle.py
+++ 03_ThreeWayAndTkinter/15_puzzle.py
@@ -3,6 +3,7 @@
 Tkinter 15 puzzle app
 '''
 import tkinter as tk
+from tkinter import messagebox
 import random
 
 class Application(tk.Frame):
@@ -19,27 +20,28 @@ class Application(tk.Frame):
 
         self.NumOfRows = 4
         self.NumOfColumns = 4
-        self.FreePuzzle = self.NumOfRows * self.NumOfColumns-1
-        self.WinReplacement = [i for i in range(self.NumOfRows*self.NumOfColumns)]
 
         self.createWidgets()
         self.createNewGame()
 
     def createWidgets(self):
         '''Create all the widgets'''
-        self.L = tk.Label(self, relief = tk.GROOVE)
+        self.L = tk.Label(self)
         self.L.grid(sticky="NEWS")
         for i in range(self.NumOfColumns):
             self.L.columnconfigure(i, weight=1)
         for i in range(self.NumOfRows):
             self.L.rowconfigure(i+1, weight=1)
-        self.N = tk.Button(self.L, text="New", command=self.createNewGame)
+        self.N = tk.Button(self.L, text = "New", command = self.createNewGame)
         self.N.grid(row = 0, column = 0, columnspan = 2)
-        self.Q = tk.Button(self.L, text="Quit", command=self.master.quit)
+        self.Q = tk.Button(self.L, text = "Quit", command = self.master.quit)
         self.Q.grid(row = 0, column = 2, columnspan = 2)
         self.Puzzles = []
-        for i in range(self.NumOfRows * self.NumOfColumns - 1):
+        for i in range(self.NumOfRows * self.NumOfColumns):
             puzzle = tk.Button(self.L)
+            def handler(p = puzzle):
+                self.movePuzzle(p)
+            puzzle['command'] = handler
             self.Puzzles.append(puzzle)
 
     def showWidgets(self):
@@ -47,19 +49,51 @@ class Application(tk.Frame):
         self.L.grid(sticky="NEWS")
         self.N.grid(row = 0, column = 0, columnspan = 2)
         self.Q.grid(row = 0, column = 2, columnspan = 2)
-        for i, puzzle in enumerate(self.Puzzles):
-            puzzle["text"] = self.Replacement[i] + 1
-            puzzle.grid(row = (i // self.NumOfRows)+1, column = i % self.NumOfColumns, sticky = "NEWS")
+        for i in self.Replacement:
+            puzzle = self.Puzzles[i]
+            row = (i // self.NumOfRows)
+            column = i % self.NumOfColumns
+            if (row == self.FreePuzzleRow and column == self.FreePuzzleColumn):
+                puzzle.grid_remove()
+            else:
+                puzzle["text"] = self.Replacement[i] + 1
+                puzzle.grid(row = row+1, column = column, sticky = "NEWS")
 
     def initNewGame(self):
+        self.FreePuzzleRow = self.NumOfRows-1
+        self.FreePuzzleColumn = self.NumOfColumns-1
         '''Create new replacement'''
-        self.Replacement = self.WinReplacement[:-1].copy()
+        self.Replacement = [i for i in range(self.NumOfRows*self.NumOfColumns-1)]
         random.shuffle(self.Replacement)
-        self.Replacement.append(self.FreePuzzle)
+        self.Replacement.append(self.NumOfRows*self.NumOfColumns-1)
 
     def createNewGame(self):
         self.initNewGame()
         self.showWidgets()
+
+    def is_sorted(self, replacement):
+        for i, e in enumerate(replacement[1:]):
+            if e <= replacement[i]:
+                return False
+        return True
+
+    def movePuzzle(self, puzzle):
+        row = puzzle.grid_info()['row']-1
+        column = puzzle.grid_info()['column']
+        if row == self.FreePuzzleRow and column == self.FreePuzzleColumn:
+            return
+
+        if (abs(row - self.FreePuzzleRow) == 1 and column == self.FreePuzzleColumn) or (abs(column - self.FreePuzzleColumn) and row == self.FreePuzzleRow):
+            puzzle_shift = row * self.NumOfRows + column
+            free_puzzle_shift = self.FreePuzzleRow * self.NumOfRows + self.FreePuzzleColumn
+
+            # Move puzzle to empty space
+            self.Replacement[puzzle_shift], self.Replacement[free_puzzle_shift] = self.Replacement[free_puzzle_shift], self.Replacement[puzzle_shift]
+            self.FreePuzzleRow, self.FreePuzzleColumn = row, column
+            self.showWidgets()
+            if (self.is_sorted(self.Replacement)):
+                messagebox.showinfo("", "You win :-)")
+                self.createNewGame()
 
 
 app = Application(title = "15 puzzle")
