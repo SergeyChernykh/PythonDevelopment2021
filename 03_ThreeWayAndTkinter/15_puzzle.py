a64a05ebe2ce8960c06d89accbcab3f6e2818fd1
SChernykh <s9160605122@gmail.com>
2021-03-14 14:53:13 +0300

    Make GUI stretchable

--- 03_ThreeWayAndTkinter/15_puzzle.py
+++ 03_ThreeWayAndTkinter/15_puzzle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 '''
-Tkinter skeleton app
+Tkinter 15 puzzle app
 '''
 import tkinter as tk
 
 class Application(tk.Frame):
-    '''Sample tkinter application class'''
 
     def __init__(self, master=None, title="<application>", **kwargs):
         '''Create root window with frame, tune weight and resize'''
@@ -13,18 +12,25 @@ class Application(tk.Frame):
         self.master.title(title)
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         self.grid(sticky="NEWS")
         self.createWidgets()
 
     def createWidgets(self):
         '''Create all the widgets'''
-        self.N = tk.Button(self, text="New", command=self.CreateNewPlacement)
-        self.N.grid(row = 0, column = 0, columnspan = 2, sticky="NEWS")
-        self.Q = tk.Button(self, text="Quit", command=self.master.quit)
-        self.Q.grid(row = 0, column = 2, columnspan = 2, sticky="NEWS")
+        self.L = tk.Label(self, relief = tk.GROOVE)
+        self.L.grid(sticky="NEWS")
+        for i in range(4):
+            self.L.columnconfigure(i, weight=1)
+            self.L.rowconfigure(i+1, weight=1)
+        self.N = tk.Button(self.L, text="New", command=self.CreateNewPlacement)
+        self.N.grid(row = 0, column = 0, columnspan = 2)
+        self.Q = tk.Button(self.L, text="Quit", command=self.master.quit)
+        self.Q.grid(row = 0, column = 2, columnspan = 2)
         self.puzzles = []
         for i in range(15):
-            puzzle = tk.Button(self, text=i+1)
+            puzzle = tk.Button(self.L, text=i+1)
             puzzle.grid(row = (i // 4)+1, column = i % 4, sticky = "NEWS")
             self.puzzles.append(puzzle)
 
