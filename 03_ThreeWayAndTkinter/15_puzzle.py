db4c315ebe27ef1567aeb2e991619cdf9956fb87
SChernykh <s9160605122@gmail.com>
2021-03-14 15:25:44 +0300

    Add logic for NEW button

--- 03_ThreeWayAndTkinter/15_puzzle.py
+++ 03_ThreeWayAndTkinter/15_puzzle.py
@@ -3,6 +3,7 @@
 Tkinter 15 puzzle app
 '''
 import tkinter as tk
+import random
 
 class Application(tk.Frame):
 
@@ -15,28 +16,51 @@ class Application(tk.Frame):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.grid(sticky="NEWS")
+
+        self.NumOfRows = 4
+        self.NumOfColumns = 4
+        self.FreePuzzle = self.NumOfRows * self.NumOfColumns-1
+        self.WinReplacement = [i for i in range(self.NumOfRows*self.NumOfColumns)]
+
         self.createWidgets()
+        self.createNewGame()
 
     def createWidgets(self):
         '''Create all the widgets'''
         self.L = tk.Label(self, relief = tk.GROOVE)
         self.L.grid(sticky="NEWS")
-        for i in range(4):
+        for i in range(self.NumOfColumns):
             self.L.columnconfigure(i, weight=1)
+        for i in range(self.NumOfRows):
             self.L.rowconfigure(i+1, weight=1)
-        self.N = tk.Button(self.L, text="New", command=self.CreateNewPlacement)
+        self.N = tk.Button(self.L, text="New", command=self.createNewGame)
         self.N.grid(row = 0, column = 0, columnspan = 2)
         self.Q = tk.Button(self.L, text="Quit", command=self.master.quit)
         self.Q.grid(row = 0, column = 2, columnspan = 2)
-        self.puzzles = []
-        for i in range(15):
-            puzzle = tk.Button(self.L, text=i+1)
-            puzzle.grid(row = (i // 4)+1, column = i % 4, sticky = "NEWS")
-            self.puzzles.append(puzzle)
+        self.Puzzles = []
+        for i in range(self.NumOfRows * self.NumOfColumns - 1):
+            puzzle = tk.Button(self.L)
+            self.Puzzles.append(puzzle)
 
-    def CreateNewPlacement(self):
-        print("new")
-        pass
+    def showWidgets(self):
+        '''Show all the widgets'''
+        self.L.grid(sticky="NEWS")
+        self.N.grid(row = 0, column = 0, columnspan = 2)
+        self.Q.grid(row = 0, column = 2, columnspan = 2)
+        for i, puzzle in enumerate(self.Puzzles):
+            puzzle["text"] = self.Replacement[i] + 1
+            puzzle.grid(row = (i // self.NumOfRows)+1, column = i % self.NumOfColumns, sticky = "NEWS")
 
-app = Application(title="15 puzzle")
+    def initNewGame(self):
+        '''Create new replacement'''
+        self.Replacement = self.WinReplacement[:-1].copy()
+        random.shuffle(self.Replacement)
+        self.Replacement.append(self.FreePuzzle)
+
+    def createNewGame(self):
+        self.initNewGame()
+        self.showWidgets()
+
+
+app = Application(title = "15 puzzle")
 app.mainloop()
