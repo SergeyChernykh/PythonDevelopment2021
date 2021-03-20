8ebc81f5f28fe6624a5aca791b2c3887e7adf7ba
SChernykh <s9160605122@gmail.com>
2021-03-20 16:35:46 +0300

    Add cursor

--- 04_PublicRepositoryEvents/LabelEdit.py
+++ 04_PublicRepositoryEvents/LabelEdit.py
@@ -3,13 +3,21 @@
 Tkinter LabelEdit app
 '''
 import tkinter as tk
+from tkinter import font
 
 class InputLabel(tk.Label):
     def __init__(self, master=None, **kwards):
         self.text = tk.StringVar()
-        super().__init__(master, textvariable = self.text, takefocus=True, highlightthickness=1, anchor="nw",**kwards)
-        self.bind("<Button-1>", lambda event: self.focus_set())
+        self.font = font.Font(family="Consolas", size=10, weight="normal")
+        super().__init__(master, font=self.font, textvariable = self.text, takefocus=True, highlightthickness=1, anchor="nw",**kwards)
+        self.Cursor = tk.Frame(self, width=2, bd = 2, relief=tk.RIDGE)
+        self.bind("<Button-1>", self.mouse_click_handler)
         self.bind('<Any-KeyPress>', self.handler)
+
+    def mouse_click_handler(self, event):
+        print(event)
+        self.focus_set()
+        self.Cursor.place(relheight = 1, x = event.x)
 
     def handler(self, event):
         print(event)
@@ -17,7 +25,6 @@ class InputLabel(tk.Label):
             self.text.set(self.text.get() + event.char)
 
 class Application(tk.Frame):
-
     def __init__(self, master=None, title="<application>", **kwargs):
         '''Create root window with frame, tune weight and resize'''
         super().__init__(master, **kwargs)
@@ -34,7 +41,7 @@ class Application(tk.Frame):
         self.L = tk.Label(self)
         self.L.grid(sticky="NEWS")
         self.IL = InputLabel(self.L)
-        self.IL.grid(row=0, column=0)
+        self.IL.grid(row=0, column=0, sticky="NEWS")
         self.Q = tk.Button(self.L, text="Quit", command=self.master.quit)
         self.Q.grid(row=1, column=0)
 
